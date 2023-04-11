be44a53ab659b7cd16f42d761f027d209877471c
Vinay Sharma <vinay02856@gmail.com>
2023-04-11 17:34:08 +0530

    refactored model

--- server/models/order.js
+++ server/models/order.js
@@ -4,38 +4,70 @@ const formSchema = new mongoose.Schema(
   {
     customerId: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
       required: true,
     },
-    firstName: String,
-    lastName: String,
-    middleName: String,
-    age: Number,
-    comments: String,
-    dob: Number,
-    gender: Number,
-    email: String,
-    phone: Number,
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    middleName: {
+      type: String,
+      default: "",
+    },
+    comments: {
+      type: String,
+      default: "",
+    },
+    dob: {
+      type: Date,
+      required: true,
+      trim: true,
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female"],
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: Number,
+      required: true,
+    },
     files: {
-      data: Buffer,
+      filename: String,
       contentType: String,
     },
-    amount: Number,
-    clientId: String,
-    work: String,
+    amount: {
+      type: Number,
+      required: true,
+    },
+    work: {
+      type: String,
+      required: true,
+    },
+    clientId: {
+      type: String,
+      required: true,
+    },
     paymentStatus: {
       type: String,
       default: "pending",
     },
     paymentAmount: {
-      type: String,
-      default: "",
+      type: Number,
+      default: 0,
     },
   },
   { timestamps: true }
 );
 
-//todo add timestamps   https://mongoosejs.com/docs/timestamps.html
 
 const Order = mongoose.model("Order", formSchema);
 export default Order;
@@ -49,73 +81,7 @@ export default Order;
 //   },
 // });
 
-// const mongoose = require("mongoose");
-
-// const orderSchema = mongoose.Schema(
-//     {
-//       user: {
-//         type: mongoose.Schema.Types.ObjectId,
-//         required: false,
-//         ref: "User",
-//       },
-//       orderItems: [
-//         {
-//           name: { type: String, required: true },
-//           amount: { type: Number, required: true },
-//           price: { type: Number, required: true },
-//           product: {
-//             type: mongoose.Schema.Types.ObjectId,
-//             required: true,
-//             ref: "Product",
-//           },
-//         },
-//       ],
-//       shippingAddress: {
-//         address: { type: String, required: true },
-//         city: { type: String, required: true },
-//         postalCode: { type: String, required: true },
-//         country: { type: String, required: true },
-//       },
-//       paymentMethod: {
-//         type: String,
-//         required: true,
-//       },
-//       paymentResult: {
-//         id: { type: String },
-//         status: { type: String },
-//         update_time: { type: String },
-//         email_address: { type: String },
-//       },
-//       shippingPrice: {
-//         type: Number,
-//         required: true,
-//         default: 0.0,
-//       },
-//       totalPrice: {
-//         type: Number,
-//         required: true,
-//         default: 0.0,
-//       },
-//       isPaid: {
-//         type: Boolean,
-//         required: true,
-//         default: false,
-//       },
-//       paidAt: {
-//         type: Date,
-//       },
-//       isDelivered: {
-//         type: Boolean,
-//         required: true,
-//         default: false,
-//       },
-//       deliveredAt: {
-//         type: Date,
-//       },
-//     },
-//     {
-//       timestamps: true,
-//     }
-//   );
-
-//   module.exports = mongoose.model("Order", orderSchema);
+//  files: [{
+//     type: mongoose.Schema.Types.ObjectId,
+//     ref: 'File'
+//   }],
