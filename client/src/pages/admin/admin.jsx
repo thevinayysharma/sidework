d3d01420b0f63e1b2bcc5bfdd3da96e1284801dc
Vinay Sharma <vinay02856@gmail.com>
2023-04-19 23:42:10 +0530

    admin commit

--- client/src/pages/admin/admin.jsx
+++ client/src/pages/admin/admin.jsx
@@ -1,23 +1,25 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./admin.css";
+import FileSaver from 'file-saver';
 
 const Admin = () => {
   const [totalSales, setTotalSales] = useState(0);
   const [currentOrders, setCurrentOrders] = useState([]);
   const [showOrders, setShowOrders] = useState(false);
+  const [selectedOrder, setSelectedOrder] = useState(null);
+
+  const REACT_APP_API_URL = "http://localhost:3000";
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        // const salesResponse = await axios.get(
-        //   "/sales"
-        // );
+        // const salesResponse = await axios.get("/sales");
         // setTotalSales(salesResponse.data);
 
         const ordersResponse = await axios.get("/orders");
-        console.log(ordersResponse);
         setCurrentOrders(ordersResponse.data);
+        console.log(ordersResponse);
       } catch (error) {
         console.log(error);
       }
@@ -25,25 +27,49 @@ const Admin = () => {
     fetchData();
   }, []);
 
-  const toggleOrders = () => {
+  const toggleShowAllOrders = () => {
     setShowOrders(!showOrders);
   };
 
-  const downloadFile = async (fileUrl) => {
-    try {
-      const response = await axios.get(fileUrl, {
-        responseType: "blob",
-      });
-
-      const url = window.URL.createObjectURL(new Blob([response.data]));
-      const link = document.createElement("a");
-      link.href = url;
-      link.download = fileUrl.substring(url.lastIndexOf("/") + 1);
-      link.click();
-    } catch (error) {
-      console.log(error);
-    }
+  const toggleIndividualDetails = (order) => {
+    setSelectedOrder(selectedOrder === order ? null : order);
   };
+
+
+  function downloadFile(filename) {
+    const url = `${REACT_APP_API_URL}/orders/${filename}`;
+  
+    fetch(url)
+      .then((response) => response.blob())
+      .then((blob) => {
+        const fileName = filename;
+        const link = document.createElement("a");
+        link.href = URL.createObjectURL(blob);
+        link.download = fileName;
+        link.click();
+      })
+      .catch((error) => {
+        console.error('Error while downloading file:', error);
+      });
+  }
+  
+  
+  // function downloadFile(filename) {
+  //   axios({
+  //     method: "GET",
+  //     url: `${REACT_APP_API_URL}/files/${filename}`,
+  //     responseType: "blob",
+  //   })
+  //     .then((response) => {
+  //       FileSaver.saveAs(response.data, "file");
+  //     })
+  //     .then(() => {
+  //       console.log("Completed");
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // }
 
   const deleteUser = async (clientId) => {
     try {
@@ -61,54 +87,70 @@ const Admin = () => {
       deleteUser(clientId);
     }
   };
-
-  const toggleDetails = () => {
-    setShowOrders(!currentOrders);
-  };
-
-  
   return (
     <div className="adminContainer">
       <div className="sales-info">
         <p>Total Sales: INR {totalSales}</p>
       </div>
-      <div className="current-orders">
+      <div className="allcurrent-orders">
         <div className="order-toggle">
-          <button onClick={toggleOrders}>
-            {showOrders ? "Hide Orders" : "Show Orders"}
+          <button onClick={toggleShowAllOrders}>
+            {showOrders ? "Hide All Orders" : "Show All Orders"}
           </button>
         </div>
         {showOrders && (
-          <div className="order-list">
-            <h4>Current Orders</h4>
+          <div className="order-list-container">
             {currentOrders.map((order) => (
               <div className="order-box" key={order.clientId}>
                 <div className="order-header">
-                  <p>Order ID: {order.clientId}</p>
-                  <p>Work: {order.work}</p>
-                  <p>First Name: {order.firstName}</p>
-                  <p>Last Name: {order.lastName}</p>
-                  <p>Middle Name: {order.middleName}</p>
-                  <p>DoB: {order.dob}</p>
-                  <p>Gender: {order.gender}</p>
-                  <p>Email: {order.email}</p>
-                  <p>Phone: {order.phone}</p>
-                  <p>Payment Status: {order.paymentStatus}</p>
                   <p>
-                    Order Amount: &#8377;{order.amount} | Paid Amount: &#8377;{" "}
-                    {order.paymentAmount}{" "}
+                    Order ID: {order.clientId}
+                    <span
+                      className="toggle-icon"
+                      onClick={() => toggleIndividualDetails(order)}
+                    >
+                      {selectedOrder === order ? "-" : "+"}
+                    </span>
                   </p>
-                  <p>Payment Status: {order.paymentStatus}</p>
-                </div>
-                <div className="order-actions">
-                  <button onClick={() => downloadFile(order.files[0].url)}>
-                    {" "}
-                    Download file{" "}
-                  </button>
-                  <button onClick={() => handleDelete(order.clientId)}>
-                    Delete
-                  </button>
-                  <button>WhatsApp </button>
+                  {selectedOrder === order && (
+                    <div>
+                      <p>Work: {order.work}</p>
+                      <p>First Name: {order.firstName}</p>
+                      <p>Last Name: {order.lastName}</p>
+                      <p>Middle Name: {order.middleName}</p>
+                      <p>DoB: {order.dob}</p>
+                      <p>Gender: {order.gender}</p>
+                      <p>Email: {order.email}</p>
+                      <p>Phone: {order.phone}</p>
+                      <p>Payment Status: {order.paymentStatus}</p>
+                      <p>
+                        Order Amount: &#8377;{order.amount} | Paid Amount:
+                        &#8377; {order.paymentAmount}{" "}
+                      </p>
+                      <p>Payment Status: {order.paymentStatus}</p>
+                      {selectedOrder && selectedOrder === order && (
+                        <div className="order-actions">
+                          <p>Attachments:</p>
+                          {order.files.map((file, index) => (
+                            <p key={index}>
+                              <button
+                                onClick={() => downloadFile(file.filename)}
+                              >
+                                Download File {index + 1}
+                              </button>
+                            </p>
+                          ))}
+                          <button
+                            className="deletebtn"
+                            onClick={() => handleDelete(order.clientId)}
+                          >
+                            Delete Order
+                          </button>
+                          <button>Whatsapp</button>
+                        </div>
+                      )}
+                    </div>
+                  )}
                 </div>
               </div>
             ))}
