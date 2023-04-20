29d1935324579985080ff94148b4f3b1a46c46d5
Vinay Sharma <vinay02856@gmail.com>
2023-04-21 00:33:19 +0530

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import Protected from "./Protected";
 import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage";
 import Admin from "./pages/admin/admin";
 import PanForm from "./pages/formspage/panform";
 import OrderDetails from "./pages/orderspage/orderdetails";
-// import Login from "../pages/Login";
-// import Register from "../pages/Register";
+import Login from "./pages/login/login";
 import Payment from "./pages/payment/payment";
 import PaymentSuccess from "./pages/payment/PaymentSuccess";
 import Footer from "./components/footer/footer";
@@ -16,6 +17,20 @@ import TermsandConditions from "./pages/t&c";
 const hideFooter = window.location.pathname === "/payment";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const loggedInUser = localStorage.getItem("loggedInUser");
+    if (loggedInUser) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
+
   return (
     <BrowserRouter>
       <div className="App">
@@ -24,15 +39,14 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/pan-apply" element={<PanForm />} />
           <Route path="/orderDetails" element={<OrderDetails />} />
-          <Route path="/admin" element={<Admin />} />
           <Route path="/payment" element={<Payment />} />
           <Route path="/paymentSuccess" element={<PaymentSuccess />} />
           <Route path="/contact" element={<ContactUs />} />
           <Route path="/epfoConsulting" element={<EPFOConsulting />} />
           <Route path="/t&c" element={<TermsandConditions />} />
-          {/* <Route path="/login" element={<Login />} /> */}
-          {/* <Route path="/register" element={<Register />} /> */}
-          <Route component={<HomePage />} />
+          <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
+          <Route path="/admin" element={ <Protected isLoggedIn={isLoggedIn}><Admin handleLogout={handleLogout} /></Protected> }/>
+          <Route path="*" element={<HomePage />} />
         </Routes>
         {!hideFooter && <Footer />}
       </div>
