db9d08c02a68c994f1585b738ed68dc461d71f96
Vinay Sharma <vinay02856@gmail.com>
2023-05-08 00:12:39 +0530

    added privacypolicy page

---
+++ client/src/pages/privacy/privacy.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import "./privacy.css";
+
+const PrivacyPolicy = () => {
+  return (
+    <div className="privacyContainer">
+      <h2>Privacy Policy</h2>
+      <p>
+        As an ecafeiNida website, we take data privacy seriously and understand the importance of protecting your personal information. Our website is designed to provide a seamless experience for users applying for Indian documents, and we only collect the data required for form filing.
+      </p>
+      <p>
+        We do not store any of your personal information or documents submitted on our website other than for the purpose of the application. Once the application is complete, your data will be deleted from our systems. We take the necessary precautions to ensure the safety and security of your information, and we do not share your data with any third-party entities.
+      </p>
+      <p>
+        To apply for Indian documents, users will be required to fill out forms that request personal information such as name, address, and other relevant details. This data will only be used for the purpose of the application and will not be shared with any unauthorized personnel.
+      </p>
+      <p>
+        Our website uses cookies to enhance your browsing experience and to improve the functionality of our website. These cookies do not collect any personal information and are used solely for the purpose of improving our website's performance.
+      </p>
+      <p>
+        In the event that we make any changes to our privacy policy, we will notify our users via email or through our website. We encourage all users to review our privacy policy regularly to stay informed of any updates or changes.
+      </p>
+      <p>
+        We take your privacy seriously and are committed to protecting your personal information. If you have any questions or concerns regarding our privacy policy or the collection and use of your personal information, please feel free to contact us.
+      </p>
+    </div>
+  );
+}
+
+export default PrivacyPolicy;
